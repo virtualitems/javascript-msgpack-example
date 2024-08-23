f80ac6c833aec87b4339d2b51b692e9c7a14e800
Alejandro CR <contacto@alejandrocr.co>
2024-08-23 06:46:09 -0500

    remove body-parser dependency

--- server.js
+++ server.js
@@ -8,19 +8,30 @@ const port = 3000;
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.raw({ type: '*/*' }));
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
 app.post('/', (req, res) => {
-  const decoded = msgpack.decode(req.body);
-  const encoded = msgpack.encode(decoded);
-  res.header('Content-Type', 'application/msgpack');
-  res.send(Buffer.from(encoded));
 
-  console.info((new Date()).toLocaleString());
-  console.info('received', req.body.byteLength);
-  console.info('sent', encoded.length, '\n');
+  const chunks = [];
+
+  req.on('data', (chunk) => {
+    chunks.push(chunk);
+  });
+
+  req.on('end', () => {
+    const body = Buffer.concat(chunks);
+    const decoded = msgpack.decode(body);
+    const encoded = msgpack.encode(decoded);
+
+    res.header('Content-Type', 'application/msgpack');
+    res.send(Buffer.from(encoded));
+
+    console.info((new Date()).toLocaleString());
+    console.info('received', body.byteLength);
+    console.info('sent', encoded.length, '\n');
+  });
+
 });
 
 app.listen(port, () => {
